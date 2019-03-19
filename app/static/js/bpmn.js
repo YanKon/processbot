1c1c5f92c3ca5808b45ff43b3a63406cefd5bdef
Yannick <yannickkonrad@icloud.com>
2019-03-19 09:53:04 +0100

    handle_bpmn

--- app/static/js/bpmn.js
+++ app/static/js/bpmn.js
@@ -1,29 +1,47 @@
-// BPMNio script
-// the diagram you are going to display
-var bpmnXML;
-
-$.ajax({
-  url: "/static/resources/testProcess.bpmn",
-  success: function (data) {
-    bpmnXML = data
-  },
-  async: false // <- this turns it into synchronous
-})      
-
 // BpmnJS is the BPMN viewer instance
-var viewer = new BpmnJS({ container: '#canvas' });
+var viewer = new BpmnJS({ container: "#canvas" });
 
-// import a BPMN 2.0 diagram
-viewer.importXML(bpmnXML, function(err) {
-  if (err) {
-    return console.error('could not import BPMN 2.0 diagram', err);
-  } else {
-    console.log("BPMN successfully imported")
-    var canvas = viewer.get('canvas');
-    // canvas.zoom('fit-viewport');
-
-    // add color
-    // canvas.addMarker('Task_03duqqg', 'highlight');
+function loadBPMN () {
+  
+  return new Promise((resolve, reject) => {
     
+    var bpmnXML;
+
+    $.ajax({
+      url: "/static/resources/testProcess.bpmn",
+      success: function(data) {
+        bpmnXML = data;
+        
+        viewer.importXML(bpmnXML, function(err) {
+          if (err) {
+            return console.error("could not import BPMN 2.0 diagram", err);
+          } else {
+            console.log("BPMN successfully imported");
+            var canvas = viewer.get("canvas");
+            resolve(canvas);
+            // canvas.zoom('fit-viewport');
+            // add color
+            // canvas.addMarker('Task_03duqqg', 'highlight');
+          }
+        });
+      },
+      async: false // <- this turns it into synchronous
+    });         
+  });
+}
+
+function handle_bpmn(responseObject) {
+
+
+  // Modell ist nicht geladen
+  if (!viewer.get("canvas").hasOwnProperty("_rootElement")) {
+    // Prozess bereits gestartet
+    if (responseObject.currentProcess !== "") {
+      loadBPMN();
+    }
   }
-});
+
+
+}
+
+
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,120 +1,165 @@
-$('#live-chat').hide(0);
+$("#live-chat").hide(0);
+
+function highlightStep(responseObject) {
+  console.log("highlight start");
+  if (responseObject.currentProcessStep !== "")
+    viewer
+      .get("canvas")
+      .addMarker(responseObject.currentProcessStep, "highlight");
+
+  if (responseObject.previousProcessStep !== "")
+    viewer.get("canvas").addMarker(responseObject.previousProcessStep, "done");
+}
+
+// Geht über alle Messages und gibt sie aus
+// 1000 * index + 500 => gibt messages mit Verzögerung aus (abhängig von index größe)
+function handle_messages(responseObject) {
+  // Bot Messages ausgeben
+  responseObject.messages.forEach(function(message, index) {
+    setTimeout(function() {
+      botui.message.bot({
+        delay: 1000,
+        loading: true,
+        content: message
+      });
+    }, 1000 * index + 500);
+  });
+}
+
+// blendet Buttons, falls vorhanden, ein
+// erkennt button klick und führt Funktion aus
+function handle_buttons(responseObject) {
+  // Buttons? --> anzeigen & geklickter Button auslesen
+  if (responseObject.buttons != []) {
+    // TODO : Eingabe Feld ausblenden
+    setTimeout(function() {
+      botui.action
+        .button({
+          action: responseObject.buttons
+        })
+        .then(function(pressedButton) {
+          // Wird ausgeführt, wenn ein Button geklickt wurde
+          submit_button(
+            responseObject.currentProcess,
+            responseObject.currentProcessStep,
+            responseObject.previousProcessStep,
+            pressedButton.value
+          );
+        });
+    }, 1000 * (responseObject.messages.length - 1) +
+      responseObject.messages.length * 500);
+  }
+}
 
 function submit_userText(userText) {
   $.post("/send_userText", { userText: userText }, handle_response);
 
   function handle_response(responseObject) {
-    
-    console.log("ResponseObject:")
+
+    handle_bpmn(responseObject)
+      .then(canvas => {
+        handle_messages(responseObject);
+        handle_buttons(responseObject);
+        highlightStep(responseObject);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+  }
+}
+
+// ResponseObject mitübergeben, damit klar ist, in welchem Prozessschritt man sich befindet
+function submit_button(
+  currentProcess,
+  currentProcessStep,
+  previousProcessStep,
+  pressedButtonValue
+) {
+  console.log("currentProcess:");
+  console.log(currentProcess);
+  console.log("currentProcessStep:");
+  console.log(currentProcessStep);
+  console.log("previousProcessStep:");
+  console.log(previousProcessStep);
+  console.log("pressedButtonValue:");
+  console.log(pressedButtonValue);
+
+  $.post(
+    "/send_button",
+    {
+      pressedButtonValue: pressedButtonValue,
+      currentProcess: currentProcess,
+      previousProcessStep: previousProcessStep,
+      currentProcessStep: currentProcessStep
+    },
+    handle_response
+  );
+
+  function handle_response(responseObject) {
+    console.log("########## im handle Response von submitButton angekommen");
+    console.log("ResponseObject:");
     console.log(responseObject);
 
     if (responseObject.currentProcessStep !== "")
-      viewer.get('canvas').addMarker(responseObject.currentProcessStep, 'highlight');
-    
+      viewer
+        .get("canvas")
+        .addMarker(responseObject.currentProcessStep, "highlight");
+
     if (responseObject.previousProcessStep !== "")
-      viewer.get('canvas').addMarker(responseObject.previousProcessStep, 'done');
-    
+      viewer
+        .get("canvas")
+        .addMarker(responseObject.previousProcessStep, "done");
 
     // Bot Messages ausgeben
-    responseObject.messages.forEach(function(message,index) {
-      setTimeout(function(){
+    responseObject.messages.forEach(function(message, index) {
+      setTimeout(function() {
         botui.message.bot({
           delay: 1000,
           loading: true,
           content: message
         });
-      },1000*index+500);
+      }, 1000 * index + 500);
     });
-    
+
     // Buttons? --> anzeigen & geklickter Button auslesen
     if (responseObject.buttons != []) {
       // TODO : Eingabe Feld ausblenden
-      setTimeout(function(){
+      setTimeout(function() {
         botui.action
           .button({
             action: responseObject.buttons
           })
           .then(function(pressedButton) {
             // Wird ausgeführt, wenn ein Button geklickt wurde
-            submit_button(responseObject.currentProcess, responseObject.currentProcessStep, responseObject.previousProcessStep, pressedButton.value);
+            submit_button(
+              responseObject.currentProcess,
+              responseObject.currentProcessStep,
+              responseObject.previousProcessStep,
+              pressedButton.value
+            );
           });
-      },1000*(responseObject.messages.length-1)+responseObject.messages.length*500);
+      }, 1000 * (responseObject.messages.length - 1) +
+        responseObject.messages.length * 500);
     }
   }
 }
-
-// ResponseObject mitübergeben, damit klar ist, in welchem Prozessschritt man sich befindet
-function submit_button(currentProcess, currentProcessStep, previousProcessStep, pressedButtonValue) {
-  console.log("currentProcess:")
-  console.log(currentProcess)
-  console.log("currentProcessStep:")
-  console.log(currentProcessStep)
-  console.log("previousProcessStep:")
-  console.log(previousProcessStep)
-  console.log("pressedButtonValue:")
-  console.log(pressedButtonValue)
-
-
-  $.post("/send_button", { pressedButtonValue: pressedButtonValue, currentProcess: currentProcess, previousProcessStep: previousProcessStep, currentProcessStep: currentProcessStep }, handle_response);
-
-  function handle_response(responseObject) {
-    console.log ("########## im handle Response von submitButton angekommen")
-    console.log("ResponseObject:")
-    console.log(responseObject);
-
-    if (responseObject.currentProcessStep !== "")
-      viewer.get('canvas').addMarker(responseObject.currentProcessStep, 'highlight');
-    
-    if (responseObject.previousProcessStep !== "")
-      viewer.get('canvas').addMarker(responseObject.previousProcessStep, 'done');
-
-    // Bot Messages ausgeben
-    responseObject.messages.forEach(function(message,index) {
-      setTimeout(function(){
-        botui.message.bot({
-          delay:1000,
-          loading: true,
-          content: message
-        });
-      },1000*index+500);
-    });
-  
-    // Buttons? --> anzeigen & geklickter Button auslesen
-    if (responseObject.buttons != []) {
-      // TODO : Eingabe Feld ausblenden
-      setTimeout(function(){
-        botui.action
-          .button({
-            action: responseObject.buttons
-          })
-          .then(function(pressedButton) {
-            // Wird ausgeführt, wenn ein Button geklickt wurde
-            submit_button(responseObject.currentProcess, responseObject.currentProcessStep, responseObject.previousProcessStep, pressedButton.value);
-          });
-      },1000*(responseObject.messages.length-1)+responseObject.messages.length*500);
-    }
-
-  }
-
-}
-
 
 // Leitet die Usereingaben ans Backend weiter
 var userText;
 $(document).ready(function() {
-  
   $("#chat_send").click(function(e) {
-      userText = $("#InputField").val();
-      submit_userText(userText);
-      console.log($("#InputField").val());
-      $("#InputField").val("");
+    userText = $("#InputField").val();
+    submit_userText(userText);
+    console.log($("#InputField").val());
+    $("#InputField").val("");
 
-        // Zeigt den UserText im Chatfenster an
-      botui.message.human({
-        content: userText
-      });
+    // Zeigt den UserText im Chatfenster an
+    botui.message.human({
+      content: userText
+    });
   });
-  
+
   $("#InputField").keypress(function(e) {
     if (e.keyCode == 13) {
       userText = $("#InputField").val();
@@ -130,20 +175,14 @@ $(document).ready(function() {
   });
 });
 
-
-
-	$('.chat-close').on('click', function(e) {
-
-		e.preventDefault();
-    $('#live-chat').fadeOut(300);
-    $('#prime').fadeIn(300);
-
-	});
-
-
-$('#prime').on('click', function(e) {
+$(".chat-close").on("click", function(e) {
   e.preventDefault();
-  $('#live-chat').fadeIn(300);
-  $('#prime').hide(0);
+  $("#live-chat").fadeOut(300);
+  $("#prime").fadeIn(300);
+});
 
+$("#prime").on("click", function(e) {
+  e.preventDefault();
+  $("#live-chat").fadeIn(300);
+  $("#prime").hide(0);
 });
