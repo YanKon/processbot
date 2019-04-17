b0ab344638ed2c6d657064571a74e05a0ddc7469
Yannick <yannickkonrad@icloud.com>
2019-04-17 20:35:10 +0200

    bpmnreader changes

--- app/utils/bpmnReader.py
+++ app/utils/bpmnReader.py
@@ -1,5 +1,4 @@
 # pylint: disable=no-member
-# https://stackoverflow.com/questions/5033547/sqlalchemy-cascade-delete
 
 import os, time, datetime, sys
 import config as con
@@ -40,12 +39,15 @@ def readBpmn(processName):
         #ProcessDoc in Datenbank, hier keine ID übergeben, weil Autoincrement
         procsesDoc = models.ProcessDoc(description = root[0][0].text, processId = processId)
         db.session.add(procsesDoc)
-        db.session.commit()
 
         sequenceFlows = []
+        detailInstructions = []
+        instructions = []
+        splitQuestions = []
+        detailDescriptions = []
+        buttonNames = []
         
         for actor in root[0].iter():
-            print(actor)
             tagType = actor.tag.split("}")[1]
 
             if (tagType == "task"):
@@ -54,44 +56,37 @@ def readBpmn(processName):
                     if len(attributeType) == 2:
                         if (attributeType[1] == "detailInstruction"):
                             detailInstruction = models.DetailInstruction(text = attribute[1], nodeId = actor.attrib['id'])
+                            detailInstructions.append(detailInstruction)
                         elif (attributeType[1] == "instruction"):
                             instruction = models.GeneralInstruction(text = attribute[1], nodeId = actor.attrib['id'])
+                            instructions.append(instruction)
                 node = models.Node(id = actor.attrib['id'], name = actor.attrib['name'], type = 'task', processId = processId)
                 db.session.add(node)
-                db.session.commit()
-                db.session.add(detailInstruction)
-                db.session.add(instruction)
-                db.session.commit()
 
             elif (tagType == "startEvent"):
                 node = models.Node(id = actor.attrib['id'], name = actor.attrib['name'], type = 'startEvent', processId = processId)
                 db.session.add(node)
-                db.session.commit()
             
             elif (tagType == "exclusiveGateway"):
                 # wenn exlusiveGateway kein Name hat <=> exclusiveGateway ist ein join
                 if "name" not in actor.attrib:
                     node = models.Node(id = actor.attrib['id'], type = 'exclusiveGateway', processId = processId)
                     db.session.add(node)
-                    db.session.commit()
 
                 else:
                     node = models.Node(id = actor.attrib['id'], name = actor.attrib['name'], type = 'exclusiveGateway', processId = processId)
                     db.session.add(node)
-                    db.session.commit()
 
                     for attribute in actor.attrib.items():
                         attributeType = attribute[0].split("}")
                         if len(attributeType) == 2:
                             if (attributeType[1] == "splitQuestion"):
                                 splitQuestion = models.SplitQuestion(text = attribute[1], nodeId = actor.attrib['id'])
-                                db.session.add(splitQuestion)
-                    db.session.commit()
+                                splitQuestions.append(splitQuestion)
 
             elif (tagType == "intermediateThrowEvent"):
                 node = models.Node(id = actor.attrib['id'], name = actor.attrib['name'], type = 'intermediateThrowEvent', processId = processId)
                 db.session.add(node)
-                db.session.commit()
 
                 for attribute in actor.attrib.items():
                     attributeType = attribute[0].split("}")
@@ -99,32 +94,33 @@ def readBpmn(processName):
                         print(attributeType[1])
                         if (attributeType[1] == "detailDescription"):
                             detailDescription = models.DetailDescription(text = attribute[1], nodeId = actor.attrib['id'])
-                            db.session.add(detailDescription)
+                            detailDescriptions.append(detailDescription)
                         elif (attributeType[1] == "button"):
                             buttonName = models.ButtonName(text = attribute[1], nodeId = actor.attrib['id'])
-                            db.session.add(buttonName)
-                db.session.commit()
+                            buttonNames.append(buttonName)
                         
             elif (tagType == "endEvent"):
                 node = models.Node(id = actor.attrib['id'], name = actor.attrib['name'], type = 'endEvent', processId = processId)
                 db.session.add(node)
-                db.session.commit()
 
             elif (tagType == "sequenceFlow"):
-                # edge = models.Edge(id = actor.attrib['id'], processId = processId, sourceId = actor.attrib['sourceRef'], targetId = actor.attrib['targetRef'])
-                sequenceFlows.append((actor.attrib['id'], processId, actor.attrib['sourceRef'], actor.attrib['targetRef']))
-                # db.session.add(edge)
-                # db.session.commit()
+                edge = models.Edge(id = actor.attrib['id'], processId = processId, sourceId = actor.attrib['sourceRef'], targetId = actor.attrib['targetRef'])
+                sequenceFlows.append(edge)
             
-        
         # ist nötig, weil bei jedem db.session.add() geprüft wird ob es die entsprechenden foreignkeys existieren!
-        # da ein Sequenceflow insbesondere aus einer sourceId und targetId besteht müsssen erste alle Nodes in der Datenbank exisiteren, daher werden die Sequeneces in einer Liste zwicshengespeichert und erst am Ende wird über diese Liste nochmal itertiert => dann alle adden und committen
-        for sequenceFlow in sequenceFlows:
-            print(sequenceFlow[0])
-            edge = models.Edge(id=sequenceFlow[0], processId=sequenceFlow[1], sourceId=sequenceFlow[2], targetId=sequenceFlow[3])
-            db.session.add(edge)
-        db.session.commit()
+        # da ein Sequenceflow insbesondere aus einer sourceId und targetId besteht müsssen erste alle Nodes in der Datenbank exisiteren, daher werden die Sequeneces in einer Liste zwicshengespeichert und erst am Ende wird über diese Liste nochmal itertiert => dann alle adden und committen        
+        def addToSession(elements):
+            for element in elements:
+                db.session.add(element)
+                
+        addToSession(sequenceFlows)    
+        addToSession(instructions)  
+        addToSession(detailInstructions)
+        addToSession(splitQuestions)
+        addToSession(detailDescriptions)
+        addToSession(buttonNames)  
 
+        db.session.commit()
                 
     except:
         e = sys.exc_info()[0]
@@ -133,3 +129,5 @@ def readBpmn(processName):
         db.session.commit()
         raise Exception("This error occured while trying to read a process Element or committing to the database:" + str(e))
         # return
+
+    
